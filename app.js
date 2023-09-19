515b811e684d264ade83e87f9a48de738aceb77d
Sohail Iran <aratavalga@gmail.com>
2023-09-18 21:27:43 -0500

    tail

---
+++ app.js
@@ -0,0 +1,23 @@
+/* function queEs() {
+    return 'Las funciones sirven para simplificar los procesos al código.';
+}
+
+console.log(queEs()); */
+
+/* Tipos de funciones */
+
+/* 1. Declarativas */
+
+/* function declarativas() {
+    console.log('Las declarativas son funciones nomales ---function() {}---.');
+}
+
+declarativas(); */
+
+/* 2. Expresion */
+
+/* function expresion() {
+    console.log('Las expresión son con una variable y con flecha al parametro. Por ejemplo: ---const x = () =>');
+}
+
+expresion(); */
